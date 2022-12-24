255551fc9cff2958b2f4eb4e6dcab63e67f293a5
Tim Kennedy <KennedyTim@outlook.com>
2022-12-23 20:14:27 -0600

    Implemented MaterialDesignExtensions to change window

--- GetMyIP/MainWindow.xaml.cs
+++ GetMyIP/MainWindow.xaml.cs
@@ -1,10 +1,8 @@
 ﻿// Copyright (c) Tim Kennedy. All Rights Reserved. Licensed under the MIT License.
 
-using System.Windows.Markup;
-
 namespace GetMyIP
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : MaterialWindow
     {
         #region NLog Instance
         private static readonly Logger _log = LogManager.GetCurrentClassLogger();
@@ -13,6 +11,8 @@ namespace GetMyIP
         #region Stopwatch
         private readonly Stopwatch _stopwatch = new();
         #endregion Stopwatch
+
+        private static SolidColorBrush _titleBrush;
 
         public MainWindow()
         {
@@ -42,9 +42,6 @@ namespace GetMyIP
             // Put the version number in the title bar
             Title = $"{AppInfo.AppName} - {AppInfo.TitleVersion}";
 
-            // Startup message in the temp file
-            _log.Info($"{AppInfo.AppName} {AppInfo.AppVersion} is starting up");
-
             // Window position
             Top = UserSettings.Setting.WindowTop;
             Left = UserSettings.Setting.WindowLeft;
@@ -52,7 +49,13 @@ namespace GetMyIP
             Width = UserSettings.Setting.WindowWidth;
             Topmost = UserSettings.Setting.KeepOnTop;
 
-            // .NET version, app framework and OS platform
+            // Log the version, build date and commit id
+            _log.Info($"{AppInfo.AppName} ({AppInfo.AppProduct}) {AppInfo.AppVersion} is starting up");
+            _log.Info($"{AppInfo.AppName} {AppInfo.AppCopyright}");
+            _log.Debug($"{AppInfo.AppName} Build date: {BuildInfo.BuildDateUtc.ToUniversalTime():f} (UTC)");
+            _log.Debug($"{AppInfo.AppName} Commit ID: {BuildInfo.CommitIDString} ");
+
+            // Log the .NET version, app framework and OS platform
             string version = Environment.Version.ToString();
             _log.Debug($".NET version: {AppInfo.RuntimeVersion.Replace(".NET", "")}  ({version})");
             _log.Debug(AppInfo.Framework);
@@ -62,10 +65,10 @@ namespace GetMyIP
             SetBaseTheme((ThemeType)UserSettings.Setting.DarkMode);
 
             // Primary color
-            SetPrimaryColor(UserSettings.Setting.PrimaryColor);
+            SetPrimaryColor((AccentColor)UserSettings.Setting.PrimaryColor);
 
             // UI size
-            double size = UIScale(UserSettings.Setting.UISize);
+            double size = UIScale((MySize)UserSettings.Setting.UISize);
             MainGrid.LayoutTransform = new ScaleTransform(size, size);
 
             // Initial page viewed
@@ -83,23 +86,23 @@ namespace GetMyIP
             _ = NavListBox.Focus();
             switch (page)
             {
-            default:
+                default:
                     _ = MainFrame.Navigate(new Page1());
                     PageTitle.Text = "Internal IP Addresses";
                     break;
-                case 1:
+                case (int)NavPage.External:
                     _ = MainFrame.Navigate(new Page2());
-                    PageTitle.Text = "External IP & Geolocation Information";
+                    PageTitle.Text = "External IP & Geolocation Info";
                     break;
-                case 3:
-                    _ = MainFrame.Navigate(new Page3());
+                case (int)NavPage.Settings:
+                    _ = MainFrame.Navigate(new SettingsPage());
                     PageTitle.Text = "Settings";
                     break;
-                case 4:
-                    _ = MainFrame.Navigate(new Page4());
+                case (int)NavPage.About:
+                    _ = MainFrame.Navigate(new AboutPage());
                     PageTitle.Text = "About";
                     break;
-                case 6:
+                case (int)NavPage.Exit:
                     Application.Current.Shutdown();
                     break;
             }
@@ -111,32 +114,91 @@ namespace GetMyIP
         }
         #endregion Navigation
 
+        #region Theme
+        /// <summary>
+        /// Gets the current theme
+        /// </summary>
+        /// <returns>Dark or Light</returns>
+        internal static string GetSystemTheme()
+        {
+            BaseTheme? sysTheme = Theme.GetSystemTheme();
+            return sysTheme != null ? sysTheme.ToString() : string.Empty;
+        }
+
+        /// <summary>
+        /// Sets the theme
+        /// </summary>
+        /// <param name="mode">Light, Dark, Darker or System</param>
+        internal void SetBaseTheme(ThemeType mode)
+        {
+            //Get the Windows accent color
+            _titleBrush = (SolidColorBrush)SystemParameters.WindowGlassBrush;
+
+            //Retrieve the app's existing theme
+            PaletteHelper paletteHelper = new();
+            ITheme theme = paletteHelper.GetTheme();
+
+            if (mode == ThemeType.System)
+            {
+                mode = GetSystemTheme()!.Equals("light", StringComparison.Ordinal) ? ThemeType.Light : ThemeType.Darker;
+            }
+
+            switch (mode)
+            {
+                case ThemeType.Light:
+                    theme.SetBaseTheme(Theme.Light);
+                    BorderBackgroundBrush = _titleBrush;
+                    theme.Paper = Colors.WhiteSmoke;
+                    break;
+                case ThemeType.Dark:
+                    theme.SetBaseTheme(Theme.Dark);
+                    BorderBackgroundBrush = _titleBrush;
+                    break;
+                case ThemeType.Darker:
+                    // Set card and paper background colors a bit darker
+                    theme.SetBaseTheme(Theme.Dark);
+                    theme.Body = (Color)ColorConverter.ConvertFromString("#FFCCCCCC");
+                    theme.Paper = (Color)ColorConverter.ConvertFromString("#FF202020");
+                    theme.CardBackground = (Color)ColorConverter.ConvertFromString("#FF141414");
+                    BorderForegroundBrush = new SolidColorBrush(theme.Body);
+                    BorderBackgroundBrush = new SolidColorBrush(theme.Paper);
+                    break;
+                default:
+                    theme.SetBaseTheme(Theme.Light);
+                    break;
+            }
+
+            //Change the app's current theme
+            paletteHelper.SetTheme(theme);
+        }
+        #endregion Theme
+
         #region Set primary color
-        private static void SetPrimaryColor(int color)
+        private static void SetPrimaryColor(AccentColor color)
         {
             PaletteHelper paletteHelper = new();
             ITheme theme = paletteHelper.GetTheme();
             PrimaryColor primary = color switch
             {
-                0 => PrimaryColor.Red,
-                1 => PrimaryColor.Pink,
-                2 => PrimaryColor.Purple,
-                3 => PrimaryColor.DeepPurple,
-                4 => PrimaryColor.Indigo,
-                5 => PrimaryColor.Blue,
-                6 => PrimaryColor.LightBlue,
-                7 => PrimaryColor.Cyan,
-                8 => PrimaryColor.Teal,
-                9 => PrimaryColor.Green,
-                10 => PrimaryColor.LightGreen,
-                11 => PrimaryColor.Lime,
-                12 => PrimaryColor.Yellow,
-                13 => PrimaryColor.Amber,
-                14 => PrimaryColor.Orange,
-                15 => PrimaryColor.DeepOrange,
-                16 => PrimaryColor.Brown,
-                17 => PrimaryColor.Grey,
-                18 => PrimaryColor.BlueGrey,
+                AccentColor.Red => PrimaryColor.Red,
+                AccentColor.Pink => PrimaryColor.Pink,
+                AccentColor.Purple => PrimaryColor.Purple,
+                AccentColor.DeepPurple => PrimaryColor.DeepPurple,
+                AccentColor.Indigo => PrimaryColor.Indigo,
+                AccentColor.Blue => PrimaryColor.Blue,
+                AccentColor.LightBlue => PrimaryColor.LightBlue,
+                AccentColor.Cyan => PrimaryColor.Cyan,
+                AccentColor.Teal => PrimaryColor.Teal,
+                AccentColor.Green => PrimaryColor.Green,
+                AccentColor.LightGreen => PrimaryColor.LightGreen,
+                AccentColor.Lime => PrimaryColor.Lime,
+                AccentColor.Yellow => PrimaryColor.Yellow,
+                AccentColor.Amber => PrimaryColor.Amber,
+                AccentColor.Orange => PrimaryColor.Orange,
+                AccentColor.DeepOrange => PrimaryColor.DeepOrange,
+                AccentColor.Brown => PrimaryColor.Brown,
+                AccentColor.Grey => PrimaryColor.Grey,
+                AccentColor.BlueGray => PrimaryColor.BlueGrey,
                 _ => PrimaryColor.Blue,
             };
             Color primaryColor = SwatchHelper.Lookup[(MaterialDesignColor)primary];
@@ -146,17 +208,27 @@ namespace GetMyIP
         #endregion Set primary color
 
         #region UI scale converter
-        private static double UIScale(int size)
+        private static double UIScale(MySize size)
         {
-            return size switch
+            switch (size)
             {
-                0 => 0.90,
-                1 => 0.95,
-                2 => 1.0,
-                3 => 1.05,
-                4 => 1.1,
-                _ => 1.0,
-            };
+                case MySize.Smallest:
+                    return 0.8;
+                case MySize.Smaller:
+                    return 0.9;
+                case MySize.Small:
+                    return 0.95;
+                case MySize.Default:
+                    return 1.0;
+                case MySize.Large:
+                    return 1.05;
+                case MySize.Larger:
+                    return 1.1;
+                case MySize.Largest:
+                    return 1.2;
+                default:
+                    return 1.0;
+            }
         }
         #endregion UI scale converter
 
@@ -209,11 +281,29 @@ namespace GetMyIP
 
         private void BtnShowMap_Click(object sender, RoutedEventArgs e)
         {
+            ShowMap();
+        }
+        #endregion PopupBox button events
+
+        #region Show Lat. Long. location in browser
+        private static void ShowMap()
+        {
             IPInfo lat = IPInfo.GeoInfoList.FirstOrDefault(x => x.Parameter == "Latitude");
             IPInfo lon = IPInfo.GeoInfoList.FirstOrDefault(x => x.Parameter == "Longitude");
-            //string url = $"https://www.latlong.net/c/?lat={lat.Value}&long={lon.Value}";
-            string url = $"https://www.google.com/maps/@{lat.Value},{lon.Value},10z";
-            //string url = $"https://www.bing.com/maps/default.aspx?cp={lat.Value}~{lon.Value}&lvl=10";
+            string url;
+            switch (UserSettings.Setting.MapProvider)
+            {
+                case (int)MapProvider.Bing:
+                    url = $"https://www.bing.com/maps/default.aspx?cp={lat.Value}~{lon.Value}&lvl=10";
+                    break;
+                case (int)MapProvider.LatLong:
+                    url = $"https://www.latlong.net/c/?lat={lat.Value}&long={lon.Value}";
+                    break;
+                default:
+                    url = $"https://www.google.com/maps/@{lat.Value},{lon.Value},10z";
+                    break;
+            }
+
             try
             {
                 using Process p = new();
@@ -232,8 +322,7 @@ namespace GetMyIP
                                     MessageBoxImage.Error);
             }
         }
-
-        #endregion PopupBox button events
+        #endregion Show Lat. Long. location in browser
 
         #region Keyboard Events
         private void Window_KeyDown(object sender, KeyEventArgs e)
@@ -254,25 +343,43 @@ namespace GetMyIP
                 {
                     switch (UserSettings.Setting.DarkMode)
                     {
-                        case 0:
-                            UserSettings.Setting.DarkMode = 1;
+                        case (int)ThemeType.Light:
+                            UserSettings.Setting.DarkMode = (int)ThemeType.Dark;
                             break;
-                        case 1:
-                            UserSettings.Setting.DarkMode = 2;
+                        case (int)ThemeType.Dark:
+                            UserSettings.Setting.DarkMode = (int)ThemeType.Darker;
                             break;
-                        case 2:
-                            UserSettings.Setting.DarkMode = 0;
+                        case (int)ThemeType.Darker:
+                            UserSettings.Setting.DarkMode = (int)ThemeType.System;
+                            break;
+                        case (int)ThemeType.System:
+                            UserSettings.Setting.DarkMode = (int)ThemeType.Light;
                             break;
                     }
                 }
+
+                if (e.Key == Key.N)
+                {
+                    if (UserSettings.Setting.PrimaryColor >= (int)AccentColor.BlueGray)
+                    {
+                        UserSettings.Setting.PrimaryColor = 0;
+                    }
+                    else
+                    {
+                        UserSettings.Setting.PrimaryColor++;
+                    }
+                }
+
                 if (e.Key == Key.Add)
                 {
                     EverythingLarger();
                 }
+
                 if (e.Key == Key.Subtract)
                 {
                     EverythingSmaller();
                 }
+
                 if (e.Key == Key.OemComma)
                 {
                     NavigateToPage(3);
@@ -311,12 +418,12 @@ namespace GetMyIP
                     break;
 
                 case nameof(UserSettings.Setting.PrimaryColor):
-                    SetPrimaryColor((int)newValue);
+                    SetPrimaryColor((AccentColor)newValue);
                     break;
 
                 case nameof(UserSettings.Setting.UISize):
                     int size = (int)newValue;
-                    double newSize = UIScale(size);
+                    double newSize = UIScale((MySize)size);
                     MainGrid.LayoutTransform = new ScaleTransform(newSize, newSize);
                     break;
             }
@@ -346,7 +453,7 @@ namespace GetMyIP
             {
                 size--;
                 UserSettings.Setting.UISize = size;
-                double newSize = UIScale(size);
+                double newSize = UIScale((MySize)size);
                 MainGrid.LayoutTransform = new ScaleTransform(newSize, newSize);
             }
         }
@@ -358,7 +465,7 @@ namespace GetMyIP
             {
                 size++;
                 UserSettings.Setting.UISize = size;
-                double newSize = UIScale(size);
+                double newSize = UIScale((MySize)size);
                 MainGrid.LayoutTransform = new ScaleTransform(newSize, newSize);
             }
         }
@@ -370,57 +477,6 @@ namespace GetMyIP
             Title = $"{AppInfo.AppName} - {AppInfo.TitleVersion}";
         }
         #endregion Window Title
-
-        #region Theme
-        /// <summary>
-        /// Gets the current theme
-        /// </summary>
-        /// <returns>Dark or Light</returns>
-        internal static string GetSystemTheme()
-        {
-            BaseTheme? sysTheme = Theme.GetSystemTheme();
-            return sysTheme != null ? sysTheme.ToString() : string.Empty;
-        }
-
-        /// <summary>
-        /// Sets the theme
-        /// </summary>
-        /// <param name="mode">Light, Dark, Darker or System</param>
-        internal static void SetBaseTheme(ThemeType mode)
-        {
-            //Retrieve the app's existing theme
-            PaletteHelper paletteHelper = new();
-            ITheme theme = paletteHelper.GetTheme();
-
-            if (mode == ThemeType.System)
-            {
-                mode = GetSystemTheme()!.Equals("light", StringComparison.Ordinal) ? ThemeType.Light : ThemeType.Dark;
-            }
-
-            switch (mode)
-            {
-                case ThemeType.Light:
-                    theme.SetBaseTheme(Theme.Light);
-                    theme.Paper = Colors.WhiteSmoke;
-                    break;
-                case ThemeType.Dark:
-                    theme.SetBaseTheme(Theme.Dark);
-                    break;
-                case ThemeType.Darker:
-                    // Set card and paper background colors a bit darker
-                    theme.SetBaseTheme(Theme.Dark);
-                    theme.CardBackground = (Color)ColorConverter.ConvertFromString("#FF141414");
-                    theme.Paper = (Color)ColorConverter.ConvertFromString("#FF202020");
-                    break;
-                default:
-                    theme.SetBaseTheme(Theme.Light);
-                    break;
-            }
-
-            //Change the app's current theme
-            paletteHelper.SetTheme(theme);
-        }
-        #endregion Theme
 
         #region Copy to clipboard and text file
         private static void CopytoClipBoard()
@@ -435,7 +491,7 @@ namespace GetMyIP
 
         private static void Copyto2TextFile()
         {
-            SaveFileDialog dialog = new()
+            Microsoft.Win32.SaveFileDialog dialog = new()
             {
                 Title = "Save",
                 Filter = "Text File|*.txt",
