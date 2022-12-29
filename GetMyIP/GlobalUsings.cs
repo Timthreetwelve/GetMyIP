b7357961730bed27eb1b60cd6f05ed11f3c4dc05
Tim Kennedy <KennedyTim@outlook.com>
2022-12-28 20:15:41 -0600

    Add logfile to settings

--- GetMyIP/GlobalUsings.cs
+++ GetMyIP/GlobalUsings.cs
@@ -20,6 +20,7 @@ global using System.Text.Json;
 global using System.Threading.Tasks;
 global using System.Windows;
 global using System.Windows.Controls;
+global using System.Windows.Data;
 global using System.Windows.Input;
 global using System.Windows.Media;
 global using System.Windows.Navigation;
--- GetMyIP/Pages/SettingsPage.xaml.cs
+++ GetMyIP/Pages/SettingsPage.xaml.cs
@@ -11,4 +11,22 @@ public partial class SettingsPage : UserControl
     {
         InitializeComponent();
     }
+
+    #region TextBox key down event
+    /// <summary>Handles the KeyDown event of the TextBox control.</summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void TextBox_KeyDown(object sender, KeyEventArgs e)
+    {
+        // Update property when enter is pressed
+        if (e.Key == Key.Enter)
+        {
+            // https://stackoverflow.com/a/13289118
+            TextBox tBox = (TextBox)sender;
+            DependencyProperty prop = TextBox.TextProperty;
+            BindingExpression binding = BindingOperations.GetBindingExpression(tBox, prop);
+            binding?.UpdateSource();
+        }
+    }
+    #endregion TextBox key down event
 }
