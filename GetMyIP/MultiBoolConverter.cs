f4aa48526c2a716597559ca46722f976ee9ac39c
Tim Kennedy <KennedyTim@outlook.com>
2023-01-21 20:23:40 -0600

    Settings page redesign

---
+++ GetMyIP/MultiBoolConverter.cs
@@ -0,0 +1,25 @@
+﻿// Copyright(c) Tim Kennedy. All Rights Reserved. Licensed under the MIT License.
+
+using System.Globalization;
+
+namespace GetMyIP;
+
+internal class MultiBoolConverter : IMultiValueConverter
+{
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        foreach (object value in values)
+        {
+            if (value is bool x && !x)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
