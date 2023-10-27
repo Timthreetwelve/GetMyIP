74bea1f16954fc9589cb33dea36c3258328b77b4
Tim Kennedy <kennedytim@outlook.com>
2023-10-26 20:31:47 -0500

    Added Dutch and Italian to defined languages list

--- GetMyIP/Models/UILanguage.cs
+++ GetMyIP/Models/UILanguage.cs
@@ -55,6 +55,8 @@ internal partial class UILanguage : ObservableObject
     {
         new UILanguage {Language = "English", LanguageCode = "en-US", LanguageNative = "English",    Contributor = "Timthreetwelve", Note="Default"},
         new UILanguage {Language = "English", LanguageCode = "en-GB", LanguageNative = "English",    Contributor = "Timthreetwelve"},
+        new UILanguage {Language = "Italian", LanguageCode = "it-IT", LanguageNative = "Italiano",   Contributor = "RB"},
+        new UILanguage {Language = "Dutch",   LanguageCode = "nl-NL", LanguageNative = "Nederlands", Contributor = "CMTriX"},
     };
 
     /// <summary>
