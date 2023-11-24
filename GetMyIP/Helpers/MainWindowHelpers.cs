db32b31c56f14ecfab5ca53d9e3b7f118e705ddc
Tim Kennedy <kennedytim@outlook.com>
2023-11-24 08:58:51 -0600

    Fix logging broken in d1e9f05

--- GetMyIP/Helpers/MainWindowHelpers.cs
+++ GetMyIP/Helpers/MainWindowHelpers.cs
@@ -43,7 +43,8 @@ internal static class MainWindowHelpers
         else
         {
             _mainWindow.Visibility = Visibility.Hidden;
-
+            string returnedJson = await IpHelpers.GetExternalInfo();
+            IpHelpers.LogIPInfo(returnedJson);
             _mainWindow.Close();
         }
     }

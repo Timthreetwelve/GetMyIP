Timthreetwelve/GetMyIP

commit d1e9f057bb3dd72fb3e7bd43aa21e9180c98f7cc

Additional work on start minimized. #22

GetMyIP/Helpers/MainWindowHelpers.cs
@@ -1,5 +1,7 @@
 ﻿// Copyright (c) Tim Kennedy. All Rights Reserved. Licensed under the MIT License.
 
+using H.NotifyIcon;
+
 namespace GetMyIP.Helpers;
 
 internal static class MainWindowHelpers
@@ -14,19 +16,38 @@ internal static class MainWindowHelpers
         if (CommandLineHelpers.ProcessCommandLine())
         {
             MainWindowUIHelpers.ApplyUISettings();
+
             string returnedJson = await IpHelpers.GetAllInfoAsync();
             IpHelpers.ProcessProvider(returnedJson);
-            EnableTrayIcon(UserSettings.Setting.MinimizeToTray);
-            if (UserSettings.Setting.StartMinimized)
+
+            if (UserSettings.Setting.StartMinimized && UserSettings.Setting.MinimizeToTray)
+            {
+                EnableTrayIcon(true);
+                WindowExtensions.Hide(_mainWindow);
+            }
+            else if (UserSettings.Setting.StartMinimized && !UserSettings.Setting.MinimizeToTray)
             {
                 _mainWindow.WindowState = WindowState.Minimized;
+                _mainWindow.Visibility = Visibility.Visible;
+                EnableTrayIcon(false);
+            }
+            else if (!UserSettings.Setting.StartMinimized && UserSettings.Setting.MinimizeToTray)
+            {
+                _mainWindow.WindowState = WindowState.Normal;
+                _mainWindow.Visibility = Visibility.Visible;
+                EnableTrayIcon(true);
+            }
+            else
+            {
+                _mainWindow.WindowState = WindowState.Normal;
+                _mainWindow.Visibility = Visibility.Visible;
+                EnableTrayIcon(false);
             }
         }
         else
         {
             _mainWindow.Visibility = Visibility.Hidden;
-            string returnedJson = await IpHelpers.GetExternalInfo();
-            IpHelpers.LogIPInfo(returnedJson);
+
             _mainWindow.Close();
         }
     }
@@ -196,6 +217,7 @@ internal static class MainWindowHelpers
     {
         if (value)
         {
+            _mainWindow.tbIcon.ForceCreate();
             _mainWindow.tbIcon.Visibility = Visibility.Visible;
             CustomToolTip.Instance.ToolTipText = ToolTipHelper.BuildToolTip();
         }

GetMyIP/GlobalUsings.cs
@@ -25,7 +25,6 @@ global using System.Windows.Data;
 global using System.Windows.Input;
 global using System.Windows.Markup;
 global using System.Windows.Media;
-global using System.Windows.Navigation;
 
 global using CommandLine;
 
@@ -39,7 +38,8 @@ global using GetMyIP.Dialogs;
 global using GetMyIP.Helpers;
 global using GetMyIP.Models;
 global using GetMyIP.ViewModels;
-global using GetMyIP.Views;
+
+global using H.NotifyIcon;
 
 global using MaterialDesignColors;
 global using MaterialDesignThemes.Wpf;

GetMyIP/ViewModels/SettingsViewModel.cs
@@ -129,7 +129,7 @@ public partial class SettingsViewModel : ObservableObject
                 }
             }
         }
-        else
+        else if (RegRun.RegRunEntry(_getmyip))
         {
             string result = RegRun.RemoveRegEntry(_getmyip);
             if (result == "OK")

commit db32b31c56f14ecfab5ca53d9e3b7f118e705ddc

Fix logging broken in d1e9f05

GetMyIP/Helpers/MainWindowHelpers.cs
@@ -43,7 +43,8 @@ internal static class MainWindowHelpers
         else
         {
             _mainWindow.Visibility = Visibility.Hidden;
-
+            string returnedJson = await IpHelpers.GetExternalInfo();
+            IpHelpers.LogIPInfo(returnedJson);
             _mainWindow.Close();
         }
     }