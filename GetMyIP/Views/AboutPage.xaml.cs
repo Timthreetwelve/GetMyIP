e9f97af02ff4479d72f45e03090e999e37cd7284
Tim Kennedy <kennedytim@outlook.com>
2023-11-26 16:04:24 -0600

    Replaced contributors Listview with Datagrid on About page

--- GetMyIP/Views/AboutPage.xaml.cs
+++ GetMyIP/Views/AboutPage.xaml.cs
@@ -11,16 +11,4 @@ public partial class AboutPage : UserControl
     {
         InitializeComponent();
     }
-
-    #region Mouse down in ListView
-    /// <summary>
-    /// Handle mouse down by doing nothing
-    /// </summary>
-    /// <param name="sender"></param>
-    /// <param name="e"></param>
-    private void ListView_PreviewMouseDown(object sender, MouseButtonEventArgs e)
-    {
-        e.Handled = true;
-    }
-    #endregion Mouse down in ListView
 }
