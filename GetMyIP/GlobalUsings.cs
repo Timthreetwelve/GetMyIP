d231eb36597dad1c72dc39f449d501593fe23ea1
Tim Kennedy <timthreetwelve@outlook.com>
2024-11-15 18:53:39 -0600

    Removed unneeded using statement.

--- GetMyIP/GlobalUsings.cs
+++ GetMyIP/GlobalUsings.cs
@@ -6,7 +6,6 @@ global using System.Collections.Generic;
 global using System.Collections.ObjectModel;
 global using System.ComponentModel;
 global using System.Diagnostics;
-global using System.Diagnostics.CodeAnalysis;
 global using System.Globalization;
 global using System.IO;
 global using System.Linq;
