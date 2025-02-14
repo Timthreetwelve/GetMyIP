59801dde1179fb356dac51808f103768557bd84e
Tim Kennedy <timthreetwelve@outlook.com>
2025-02-14 15:00:13 -0600

    Removed code used for testing flag icon change.

--- GetMyIP/ViewModels/NavigationViewModel.cs
+++ GetMyIP/ViewModels/NavigationViewModel.cs
@@ -346,25 +346,6 @@ internal sealed partial class NavigationViewModel : ObservableObject
                         _mainWindow!.NavigationListBox.SelectedValue = FindNavPage(NavPage.About);
                         break;
                     }
-                case Key.F2:
-                    {
-                        //  ____                                 _____ _     _     
-                        // |  _ \ ___ _ __ ___   _____   _____  |_   _| |__ (_)___ 
-                        // | |_) / _ \ '_ ` _ \ / _ \ \ / / _ \   | | | '_ \| / __|
-                        // |  _ <  __/ | | | | | (_) \ V /  __/   | | | | | | \__ \
-                        // |_| \_\___|_| |_| |_|\___/ \_/ \___|   |_| |_| |_|_|___/
-                        //
-                        //  ____        __                  ____      _                     
-                        // | __ )  ___ / _| ___  _ __ ___  |  _ \ ___| | ___  __ _ ___  ___ 
-                        // |  _ \ / _ \ |_ / _ \| '__/ _ \ | |_) / _ \ |/ _ \/ _` / __|/ _ \
-                        // | |_) |  __/  _| (_) | | |  __/ |  _ <  __/ |  __/ (_| \__ \  __/
-                        // |____/ \___|_|  \___/|_|  \___| |_| \_\___|_|\___|\__,_|___/\___|
-
-                        TrayIconHelpers.CurrentCountryCode = "NL";
-                        _mainWindow!.TbIcon.Icon = TrayIconHelpers.GetCountryFlag("NL");
-                        RefreshInfo.Instance.LastIPAddress = "127.0.0.1";
-                        break;
-                    }
                 case Key.F5:
                     {
                         _ = RefreshIpInfo();
