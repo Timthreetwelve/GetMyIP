d41e54fbe6a55374335d63b6543bd2a12799c951
Tim Kennedy <timthreetwelve@outlook.com>
2026-06-03 19:00:05 -0500

    Fix translation percent exceeding 100%.

--- GetMyIP/Helpers/ResourceHelpers.cs
+++ GetMyIP/Helpers/ResourceHelpers.cs
@@ -103,6 +103,7 @@ internal static class ResourceHelpers
                 return GetStringResource("MsgText_Error_Caption");
             }
             double percent = (double)dictionary.Count / totalCount;
+            percent = Math.Min(percent, 1.0);  // Cap at 100%
             percent = Math.Round(percent, 2, MidpointRounding.ToZero);
             return percent.ToString("P0", CultureInfo.InvariantCulture);
         }
