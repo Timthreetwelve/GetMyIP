a062df19b8b65845c6a9f37c41faf753b3a4f564
Tim Kennedy <timthreetwelve@outlook.com>
2026-07-03 19:59:30 -0500

    Update build info to use commit date, remove PS script
    Replaced build date with commit date throughout the app, including logs, About page, and resource strings. Removed PowerShell script and related MSBuild target for BuildInfo.cs generation; BuildInfo.cs is now tracked directly and includes commit date (UTC/local). Updated .gitignore and project file accordingly.

---
+++ GetMyIP/BuildInfo.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Tim Kennedy. All Rights Reserved. Licensed under the MIT License.
+
+namespace GetMyIP;
+
+internal static class BuildInfo
+{
+    public static readonly string CommitIDString = VersionInfo.GitRevShort;
+
+    public static readonly string CommitIDFullString = VersionInfo.GitRevLong;
+
+    public static readonly string? Prerelease = VersionInfo.VersionPrerelease;
+
+    public static readonly DateTime CommitDateUtc =
+    DateTime.Parse(VersionInfo.GitCommitterDate, CultureInfo.InvariantCulture,
+        DateTimeStyles.AdjustToUniversal);
+
+    public static string CommitDateStringUtc = $"{CommitDateUtc:f} (UTC)";
+    public static string CommitDateStringLocal = $"{CommitDateUtc.ToLocalTime():f} (Local)";
+
+    public static readonly string VersionString = string.IsNullOrWhiteSpace(Prerelease)
+        ? VersionInfo.Version
+        : $"{VersionInfo.Version}-{Prerelease}";
+}
