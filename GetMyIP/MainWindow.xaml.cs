0485295349f699d9e274dd6be9205c3073602322
Tim Kennedy <timthreetwelve@outlook.com>
2026-08-25 18:27:23 -0500

    Discard duplicate snackbar messages
    - Fix for duplicate messages as a result of the addition of the QueueMessageNoClear method in #266

--- GetMyIP/MainWindow.xaml.cs
+++ GetMyIP/MainWindow.xaml.cs
@@ -15,5 +15,7 @@ public partial class MainWindow
         InitializeComponent();
 
         _ = MainWindowHelpers.GetMyIPStartUp();
+
+        SnackBar1.MessageQueue!.DiscardDuplicates = true;
     }
 }
